ba17fa8e807f1056f2c26933c22f7b0f74582392
Iehtu <ggenerator@ya.ru>
2021-05-24 19:50:19 +0300

    Create Spring Boot

---
+++ src/main/java/ru/iehtu/chucknorrisdemo/ChucknorrisdemoApplication.java
@@ -0,0 +1,13 @@
+package ru.iehtu.chucknorrisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/iehtu/chucknorrisdemo/ChucknorrisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iehtu.chucknorrisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChucknorrisdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
